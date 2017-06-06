442a61d07f47ca8ef37d19d50d6a5746c7edd2a8
Sean Curtis <sean.curtis@tri.global>
2017-06-06 14:32:23 -0700

    Refactoring on GeometryContext
    1) GeometryContext provides syntactic sugar for extracting the
       GeometryState.
    2) Use GeometryWorld to create GeometryContext.
    3) Make GeometryWorld accept GeometryContext instead of
       GeometryStatus. (Including modification of GeometryWorld unit tests.)
    
    Compromises made:
    
    1) GeometryState now generates the source ids; that means to determine if
       a source is registered, it *absolutely* requires a state/context.
    2) Overloaded GeometryWorld::GetFrameKinematicsSet - one takes a context
       the other a state. This facilitates the GeometrySystem for interacting
       with the FrameKinematicsSet *before* a context has been generated.
    -- The two are related; if World can validate sources on its own, then
       System and World

--- drake/geometry/geometry_context.h
+++ drake/geometry/geometry_context.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include "drake/geometry/geometry_state.h"
 #include "drake/systems/framework/leaf_context.h"
+
 namespace drake {
 namespace geometry {
 
+/** The custom leaf context type for GeometrySystem and GeometryWorld. */
 template <typename T>
-class GeometryContext : public drake::systems::LeafContext<T> {};
+class GeometryContext : public drake::systems::LeafContext<T> {
+ public:
+  /** Returns a mutable reference of the underlying geometry state. */
+  GeometryState<T>& get_mutable_geometry_state();
+
+  /** Returns a const reference of the underlying geometry state. */
+  const GeometryState<T>& get_geometry_state() const;
+};
 
 }  // namespace geometry
 }  // namespace drake
