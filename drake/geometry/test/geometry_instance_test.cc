897f3cbc20657b6bebf6a6b775f902be25e3a313
Sean Curtis <sean.curtis@tri.global>
2017-08-22 15:17:30 -0700

    Culled some zombie code from geometry_instance_test

--- drake/geometry/test/geometry_instance_test.cc
+++ drake/geometry/test/geometry_instance_test.cc
@@ -1,17 +1,12 @@
 #include "drake/geometry/geometry_instance.h"
 
-#include <memory>
-
 #include <gtest/gtest.h>
 
 #include "drake/common/copyable_unique_ptr.h"
-#include "drake/common/eigen_types.h"
 
 namespace drake {
 namespace geometry {
 namespace {
-
-using Pose = Isometry3<double>;
 
 // Confirms that the instance is copyable.
 GTEST_TEST(GeometryInstanceTest, IsCopyable) {
