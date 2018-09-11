b51d8df6084999f75603d12f2f5ff270b1d96048
Sean Curtis <sean.curtis@tri.global>
2018-09-11 11:23:19 -0700

    Adding geometry properties
    This adds the common, underlying functionality. (No virtual methods,
    however). Each typed property set will inherit from this to pick up all of
    the set logic.

---
+++ geometry/geometry_properties.cc
@@ -0,0 +1,9 @@
+#include "drake/geometry/geometry_properties.h"
+
+namespace drake {
+namespace geometry {
+
+const std::string GeometryProperties::kDefaultGroup("__^default^__");
+
+}  // namespace geometry
+}  // namespace drake
