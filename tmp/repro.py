e0ac8867e3e0427fee1ba18e2ca323fd074af5fe
Eric Cousineau <eric.cousineau@tri.global>
2021-10-14 12:32:22 -0400

    shrink num explict deps; still fails

--- tmp/repro.py
+++ tmp/repro.py
@@ -2,6 +2,4 @@ assert __name__ == "__main__"
 
 from pydrake.geometry import DrakeVisualizer, SceneGraph
 from pydrake.systems.framework import DiagramBuilder
-builder = DiagramBuilder()
-scene_graph = builder.AddSystem(SceneGraph())
-DrakeVisualizer.AddToBuilder(builder, scene_graph)
+DrakeVisualizer.AddToBuilder(DiagramBuilder(), SceneGraph())
