31a558ee87ee8a6c05b2ac04e5bf82d71c0d862d
Sean Curtis <sean.curtis@tri.global>
2024-06-25 13:25:46 -0700

    An initial attempt to refactor
    This apparently messes up the compiler enough that it's not worth doing.
    It slows the implementation by a good 10% or more.

--- geometry/proximity/boxes_overlap.cc
+++ geometry/proximity/boxes_overlap.cc
@@ -48,6 +48,24 @@ VecType abs4(VecType x) {
   return hn::AndNot(hn::Set(tag, -0.0), x);
 }
 
+template <typename VecT>
+struct BroadcastVector {
+  BroadcastVector(const hn::FixedTag<double, 4> tag, const Vector3d& v)
+      : first(hn::Set(tag, v[0])),
+        second(hn::Set(tag, v[1])),
+        third(hn::Set(tag, v[2])) {}
+
+  VecT first;
+  VecT second;
+  VecT third;
+};
+
+// template <typename VecT>
+// class EdgeSeparationDetector {
+//  public:
+//   EdgeSeparationDetector(const VecT& stu0, const BroadcastVector& pqr)
+// };
+
 }  // namespace
 
 // This implementation follows section 64 (on PDF page 102) of:
@@ -70,12 +88,14 @@ bool BoxesOverlap(const Vector3d& half_size_a, const Vector3d& half_size_b,
   const hn::FixedTag<double, 4> tag;
   using VecT = hn::Vec<decltype(tag)>;
 
-  const VecT eps4 = hn::Set(tag, kEpsilon);
   const VecT xyz0 = hn::LoadN(tag, xyz, 3);
   const VecT pqr0 = hn::LoadN(tag, pqr, 3);
-  const VecT ssss = hn::Set(tag, stu[0]);
-  const VecT tttt = hn::Set(tag, stu[1]);
-  const VecT uuuu = hn::Set(tag, stu[2]);
+  const VecT stu0 = hn::LoadN(tag, stu, 3);
+  const BroadcastVector<VecT> pqr_b(tag, half_size_a);
+  const BroadcastVector<VecT> stu_b(tag, half_size_b);
+  const BroadcastVector<VecT> xyz_b(tag, p_AB);
+
+  const VecT eps4 = hn::Set(tag, kEpsilon);
   const VecT abc0 = hn::LoadN(tag, R, 3);
   const VecT def0 = hn::LoadN(tag, R + 3, 3);
   const VecT ghi0 = hn::LoadN(tag, R + 6, 3);
@@ -85,22 +105,14 @@ bool BoxesOverlap(const Vector3d& half_size_a, const Vector3d& half_size_b,
 
   // First category of cases separating along a's axes.
   VecT left = abs4(xyz0);
-  VecT right = pqr0;                      //  p   q   r   _
-  right = hn::MulAdd(ssss, ABC_, right);  // +sA +sB +sC  _
-  right = hn::MulAdd(tttt, DEF_, right);  // +tD +tE +tF  _
-  right = hn::MulAdd(uuuu, GHI_, right);  // +uG +uG +uI  _
+  VecT right = pqr0;                               //  p   q   r   _
+  right = hn::MulAdd(stu_b.first, ABC_, right);    // +sA +sB +sC  _
+  right = hn::MulAdd(stu_b.second, DEF_, right);   // +tD +tE +tF  _
+  right = hn::MulAdd(stu_b.third, GHI_, right);    // +uG +uG +uI  _
   auto cmp = hn::Gt(left, right);
   if (!hn::AllFalse(tag, cmp) && hn::FindFirstTrue(tag, cmp) < 3) {
     return false;
   }
-
-  const VecT stu0 = hn::LoadN(tag, stu, 3);
-  const VecT pppp = hn::Set(tag, pqr[0]);
-  const VecT qqqq = hn::Set(tag, pqr[1]);
-  const VecT rrrr = hn::Set(tag, pqr[2]);
-  const VecT xxxx = hn::Set(tag, xyz[0]);
-  const VecT yyyy = hn::Set(tag, xyz[1]);
-  const VecT zzzz = hn::Set(tag, xyz[2]);
 
   // Transpose R_AB to R_BA.
   const VecT abcd = hn::LoadU(tag, R);
@@ -119,14 +131,14 @@ bool BoxesOverlap(const Vector3d& half_size_a, const Vector3d& half_size_b,
   const VecT CFI_ = hn::Add(abs4(cfi_), eps4);
 
   // Second category of cases separating along b's axes.
-  left = hn::Mul(xxxx, adg_);           //  xa  xd  xg  _
-  left = hn::MulAdd(yyyy, beh_, left);  // +yb +ye +th  _
-  left = hn::MulAdd(zzzz, cfi_, left);  // +zc +zf +zi  _
+  left = hn::Mul(xyz_b.first, adg_);            //  xa  xd  xg  _
+  left = hn::MulAdd(xyz_b.second, beh_, left);  // +yb +ye +th  _
+  left = hn::MulAdd(xyz_b.third, cfi_, left);   // +zc +zf +zi  _
   left = abs4(left);
-  right = stu0;                           //  s   t   u   _
-  right = hn::MulAdd(pppp, ADG_, right);  // +pA +pD +pG  _
-  right = hn::MulAdd(qqqq, BEH_, right);  // +pB +pE +pH  _
-  right = hn::MulAdd(rrrr, CFI_, right);  // +rC +rF +rI  _
+  right = stu0;                                   //  s   t   u   _
+  right = hn::MulAdd(pqr_b.first, ADG_, right);   // +pA +pD +pG  _
+  right = hn::MulAdd(pqr_b.second, BEH_, right);  // +qB +qE +qH  _
+  right = hn::MulAdd(pqr_b.third, CFI_, right);   // +rC +rF +rI  _
   cmp = hn::Gt(left, right);
   if (!hn::AllFalse(tag, cmp) && hn::FindFirstTrue(tag, cmp) < 3) {
     return false;
@@ -142,11 +154,11 @@ bool BoxesOverlap(const Vector3d& half_size_a, const Vector3d& half_size_b,
   // products of a's and b's axes. We'll do it in three groups, for i={0,1,2}.
 
   // i == 0
-  left = hn::Mul(zzzz, beh_);
-  left = hn::NegMulAdd(yyyy, cfi_, left);
+  left = hn::Mul(xyz_b.third, beh_);
+  left = hn::NegMulAdd(xyz_b.second, cfi_, left);
   left = abs4(left);
-  right = hn::Mul(qqqq, CFI_);
-  right = hn::MulAdd(rrrr, BEH_, right);
+  right = hn::Mul(pqr_b.second, CFI_);
+  right = hn::MulAdd(pqr_b.third, BEH_, right);
   right = hn::MulAdd(tus_, GAD_, right);
   right = hn::MulAdd(ust_, DGA_, right);
   cmp = hn::Gt(left, right);
@@ -158,11 +170,11 @@ bool BoxesOverlap(const Vector3d& half_size_a, const Vector3d& half_size_b,
   const VecT HBE_ = hn::Per4LaneBlockShuffle<3, 1, 0, 2>(BEH_);
 
   // i == 1
-  left = hn::Mul(xxxx, cfi_);
-  left = hn::NegMulAdd(zzzz, adg_, left);
+  left = hn::Mul(xyz_b.first, cfi_);
+  left = hn::NegMulAdd(xyz_b.third, adg_, left);
   left = abs4(left);
-  right = hn::Mul(rrrr, ADG_);
-  right = hn::MulAdd(pppp, CFI_, right);
+  right = hn::Mul(pqr_b.third, ADG_);
+  right = hn::MulAdd(pqr_b.first, CFI_, right);
   right = hn::MulAdd(tus_, HBE_, right);
   right = hn::MulAdd(ust_, EHB_, right);
   cmp = hn::Gt(left, right);
@@ -174,11 +186,11 @@ bool BoxesOverlap(const Vector3d& half_size_a, const Vector3d& half_size_b,
   const VecT ICF_ = hn::Per4LaneBlockShuffle<3, 1, 0, 2>(CFI_);
 
   // i == 2
-  left = hn::Mul(yyyy, adg_);
-  left = hn::NegMulAdd(xxxx, beh_, left);
+  left = hn::Mul(xyz_b.second, adg_);
+  left = hn::NegMulAdd(xyz_b.first, beh_, left);
   left = abs4(left);
-  right = hn::Mul(pppp, BEH_);
-  right = hn::MulAdd(qqqq, ADG_, right);
+  right = hn::Mul(pqr_b.first, BEH_);
+  right = hn::MulAdd(pqr_b.second, ADG_, right);
   right = hn::MulAdd(tus_, ICF_, right);
   right = hn::MulAdd(ust_, FIC_, right);
   cmp = hn::Gt(left, right);
